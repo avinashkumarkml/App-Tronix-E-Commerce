332a6ee11190705736380d8a882303dafdc77956
Avinash <96098283+avinashkumarkml@users.noreply.github.com>
2023-07-20 20:51:35 +0530

    update my final project

---
+++ mobile/src/components/About.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+
+function About() {
+  return (
+    <>
+      <div className="container py-5 my-5">
+        <div className="row">
+          <div className="col-mid-6">
+            <h1>About.us</h1>
+            <p className="lead">
+              Aptronix is India’s Largest Apple Premium Reseller. Started in
+              2015 at the Begumpet store in Bihar with just 10 employees,
+              Aptronix is now Apple's biggest partner with 55 stores, 21 service
+              centres and 500+ employees, across 14 cities in India. Aptronix
+              offers the complete range of Apple products along with accessories
+              and service to match. Aptronix is an entity of Premium Lifestyle &
+              Fashion India Private Limited (PLFIPL). PLFIPL is known for its
+              wide network of luxury retail and distribution brands like Calvin
+              Klein, Dyson, Max, Reliance Jio, Luxittoca to name a few
+            </p>
+            <h1>Aptronix Vision:</h1>
+            <p>
+              To create a bond between apple and its customers and giving them a
+              seamless retail experience
+            </p>
+            <h1>values</h1>
+            <p>Be remarkable</p>
+            <p>Introspect</p>
+            <p>Take ownership</p>
+            <p>Be Curious</p>
+            <p>Execution matters</p>
+            <p>Collaborate</p>
+          </div>
+          <div className="col-md-6">
+            <img height="600px" width="1300px" src="https://photos5.appleinsider.com/gallery/40572-93408-iPhone-14-bw-xl.jpg" alt="" />
+          </div>
+        </div>
+      </div>
+    
+    </>
+  );
+}
+
+export default About;
